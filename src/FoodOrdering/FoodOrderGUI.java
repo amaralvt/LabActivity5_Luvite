a10827bbe0ad867e158271b353bebda43e24fedd
amaralvt <ariannemarieangela.luvite@cit.edu>
2022-12-17 20:23:31 +0800

    Add FoodOrdering main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,9 +2,13 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
     private JCheckBox cPizza, cBurger, cFries, cSoftDrinks, cTea, cSundae;
     private JRadioButton rb5, rb10, rb15, rbNone;
     private JButton btnOrder;
+
+    public static void main(String[] args) {
+
+    }
 }
