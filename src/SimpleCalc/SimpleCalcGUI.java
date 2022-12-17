6ab5d908853cdca8d8d91ef258087f277e0d21b8
amaralvt <ariannemarieangela.luvite@cit.edu>
2022-12-17 19:49:46 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,7 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -9,8 +10,49 @@ public class SimpleCalcGUI extends JFrame {
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JLabel lblResult;
+    private JTextField tfResult;
 
+    public SimpleCalcGUI () {
+
+        for (String s : Arrays.asList("+", "-", "*", "/")) {
+            cbOperations.addItem(s);
+        }
+
+        btnCompute.addActionListener(e -> computeResult());
+    }
     public static void main(String[] args) {
+        SimpleCalcGUI calc = new SimpleCalcGUI();
+        calc.setContentPane(calc.panel1);
+        calc.setSize(600,600);
+        calc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        calc.setVisible(true);
+
+        new SimpleCalcGUI();
+    }
+
+    private void computeResult() {
+        int number1 = Integer.parseInt(tfNumber1.getText());
+        int number2 = Integer.parseInt(tfNumber2.getText());
+        char operator = cbOperations.getSelectedItem().toString().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+        }
+
+            tfResult.setText(String.valueOf((result)));
 
     }
+
 }
